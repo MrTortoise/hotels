42850ef3d7307fc932d495ce1ddd39a927dcc3ed
John Nicholas <themrtortoise@gmail.com>
2018-10-01 09:58:27 +0100

    create-react-app and added basic getHotelsAdapter with happy path

---
+++ src/hotelSearch/hotelSearchResultsAdapter.spec.js
@@ -0,0 +1,25 @@
+const getHotels = () => {
+  return [
+    {
+      "name": "hotelone",
+      "starRating": 5,
+      "facilities": ["car park", "pool"]
+    },
+    {
+      "name": "hoteltwo",
+      "starRating": 3,
+      "facilities": ["car park", "gym"]
+    },
+    {
+      "name": "hotelthree",
+      "starRating": 3,
+      "facilities": []
+    }
+  ]
+}
+
+describe('hotel results adapter', ()=>{
+  it('will return 3 hotels', ()=>{
+    expect(getHotels()).toHaveLength(3)
+  })
+})
