748594319bd8a495425011f67c6b89d5f74cb10a
John Nicholas <themrtortoise@gmail.com>
2018-10-01 13:41:18 +0100

    added hotel stars, renamed css to wrapper

--- src/hotelSearch/hotel.spec.js
+++ src/hotelSearch/hotel.spec.js
@@ -10,18 +10,21 @@ const hotelName = 'hotelOne'
 const stars = 4
 const facilities = ["car park", "pool"]
 
+const hotel_name = '.hotel-name';
+
   var wrap;
+const hotel_stars = '.hotel-stars-wrapper';
 describe('a hotel should', ()=>{
     beforeEach(()=>{
       wrap = shallow(<Hotel name={hotelName} stars={stars} />)
     })
 
-    it('have a name', ()=>{
-        expect(wrap.find('.hotel-name').length).toBe(1)
+    it('have a name', ()=>{        
+        expect(wrap.find(hotel_name).length).toBe(1)
     })
 
     it('have a rating', ()=>{
-        expect(wrap.find('.hotel-stars').length).toBe(1)
+        expect(wrap.find(hotel_stars).length).toBe(1)
     })    
 
     it('have the  facilities', ()=>{
@@ -29,10 +32,10 @@ describe('a hotel should', ()=>{
     })
 
     it('should have the right name', ()=>{
-        expect(wrap.find('.hotel-name').text()).toBe(hotelName)
+        expect(wrap.find(hotel_name).text()).toBe(hotelName)
     })
 
     it('should have the right star rating', ()=>{
-        expect(wrap.find('.hotel-stars').text()).toEqual(stars.toString())
+        expect(wrap.find(hotel_stars).text()).toEqual(stars.toString())
     })
 })
