24047c6e9919891ce4cdc01c0ec4f668e80f1332
John Nicholas <themrtortoise@gmail.com>
2018-10-01 12:07:34 +0100

    e2e for hotel search element

--- src/App.e2e.js
+++ src/App.e2e.js
@@ -1,11 +1,18 @@
 const expect = require('chai').expect;
 
-describe('TodoList App', () => {
-  it('Should load with the right title', () => {
-    browser.url('http://localhost:3000/');
-    console.log(browser.getTitle())
-    const actualTitle = browser.getTitle();
 
+describe('TodoList App', () => {
+  beforeEach(()=>{
+    browser.url('http://localhost:3000/');
+  })
+  it('Should load with the right title', () => {
+    
+    const actualTitle = browser.getTitle();
     expect(actualTitle).to.eql('Hotel Search');
   });
+
+  it('Should have the Hotel Search Component', ()=>{
+    var ele = browser.element('.hotel-search')
+    expect(ele.type).to.not.equal('NoSuchElement')
+  })
 });
