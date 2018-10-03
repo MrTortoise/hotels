58e33839c32d137f779865a6e720c25b13f7441e
John Nicholas <themrtortoise@gmail.com>
2018-10-03 20:42:52 +0100

    added get hotels reducer and action test

--- src/hotelSearch/getHotelSearchResults/getHotelSearchResults.js
+++ src/hotelSearch/getHotelSearchResults/getHotelSearchResults.js
@@ -1,53 +1,6 @@
-export const FETCH_HOTELS_BEGIN   = 'FETCH_HOTELS_BEGIN';
-export const FETCH_HOTELS_SUCCESS = 'FETCH_HOTELS_SUCCESS';
-export const FETCH_HOTELS_FAILURE = 'FETCH_HOTELS_FAILURE';
-
-export const fetchProductsBegin = () => ({
-  type: FETCH_HOTELS_BEGIN
-});
-
-export const fetchHotelsSuccess = hotels => ({
-  type: FETCH_HOTELS_SUCCESS,
-  payload: { hotels }
-});
-
-export const fetchHotelsError = error => ({
-  type: FETCH_HOTELS_FAILURE,
-  payload: { error }
-});
-
-const initialState = {
-  hotelSearchResults: [],
-  loading: false,
-  error: null
-};
-
-
-export const hotelReducer = (state = initialState, action) => {
-  switch(action.type) {
-    case FETCH_HOTELS_BEGIN:
-    return {
-      ...state,
-      loading: true,
-      error: null
-    };
-    
-    case FETCH_HOTELS_SUCCESS:
-    return {
-      ...state,
-      loading: false,
-      hotelSearchResults: action.payload.products
-    };
-    
-    case FETCH_HOTELS_FAILURE:
-    return {
-      ...state,
-      loading: false,
-      error: action.payload.error,
-      hotelSearchResults: []
-    };
-    
-    default:
-    return state;
-  }
+export const getHotelSearchResultsAction = () =>{
+  type: GET_HOTELS
 }
+
+export const GET_HOTELS = 'GET_HOTELS'
+
--- src/hotelSearch/getHotelSearchResults/getHotelSearchResults.spec.js
+++ src/hotelSearch/getHotelSearchResults/getHotelSearchResults.spec.js
@@ -1,5 +1,13 @@
-describe("Get hotels should", ()=>{
-  it('do something', ()=>{
-    
+import {hotels} from './hotelSearchResultsAdapter'
+import {getHotelSearchResultsReducer, getHotelSearchResultsAction} from './hotelSearchResultsAdapter'
+
+describe("Get hotels will", ()=>{
+  const state = {}
+  it('have a reducer that returns all hotels', ()=>{
+
+    const result = getHotelSearchResultsReducer({
+      ...state,getHotelSearchResultsAction
+    })
+    expect(result.hotelSearchResults).toEqual(hotels)    
   })
 })
