a06324503d17cc773a65cdab6fb8943f071426fc
John Nicholas <themrtortoise@gmail.com>
2018-10-03 20:58:51 +0100

    action and reducer for get hotels

--- src/hotelSearch/getHotelSearchResults/getHotelSearchResults.js
+++ src/hotelSearch/getHotelSearchResults/getHotelSearchResults.js
@@ -1,6 +1,18 @@
-export const getHotelSearchResultsAction = () =>{
+import {hotels} from './hotelSearchResultsAdapter'
+
+export const getHotelSearchResultsAction = () => {return {
   type: GET_HOTELS
-}
+}}
 
 export const GET_HOTELS = 'GET_HOTELS'
 
+export const getHotelSearchResultsReducer = (state = {}, action) =>{
+  switch (action.type){
+    case GET_HOTELS:{
+      return {
+        ...state,
+        hotelSearchResults: hotels
+      }
+    }
+  }
+}
--- src/hotelSearch/getHotelSearchResults/getHotelSearchResults.spec.js
+++ src/hotelSearch/getHotelSearchResults/getHotelSearchResults.spec.js
@@ -1,13 +1,11 @@
 import {hotels} from './hotelSearchResultsAdapter'
-import {getHotelSearchResultsReducer, getHotelSearchResultsAction} from './hotelSearchResultsAdapter'
+import {getHotelSearchResultsReducer, getHotelSearchResultsAction} from './getHotelSearchResults'
 
 describe("Get hotels will", ()=>{
   const state = {}
   it('have a reducer that returns all hotels', ()=>{
-
-    const result = getHotelSearchResultsReducer({
-      ...state,getHotelSearchResultsAction
-    })
+    const action = getHotelSearchResultsAction()
+    const result = getHotelSearchResultsReducer({}, action)
     expect(result.hotelSearchResults).toEqual(hotels)    
   })
 })
